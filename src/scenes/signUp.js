22c4246ae5b645dc8a905681bb80745240fd7186
EvilMangos <59839253+EvilMangos@users.noreply.github.com>
2022-01-30 16:28:47 +0200

    CB-224: add cancel button for sign up

--- src/scenes/signUp.js
+++ src/scenes/signUp.js
@@ -2,14 +2,36 @@ const {Scenes, Markup} = require('telegraf');
 const authValidator = require('../utils/authValidation');
 const User = require('../services/user/index');
 const setKeyboard = require('../utils/setKeyboard');
+const keyboardData = require('../utils/keyboardData.json');
+
+const defaultKeyboard = async (ctx) => {
+    await ctx.telegram.sendMessage(ctx.chat.id, 'Sign up is canceled', {
+        reply_markup: {
+            resize_keyboard: true,
+            keyboard: keyboardData.guest
+        }
+    });
+    return ctx.scene.leave();
+}
 
 module.exports = new Scenes.WizardScene(
     'sign-up',
     async (ctx) => {
+        await ctx.telegram.sendMessage(ctx.chat.id, 'SignUp', {
+            reply_markup: {
+                resize_keyboard: true,
+                keyboard: [
+                    [
+                        {text: 'Cancel'}
+                    ]
+                ]
+            }
+        });
         await ctx.reply('Enter your first name:');
         return ctx.wizard.next();
     },
     async (ctx) => {
+        if (ctx.message.text === 'Cancel') return defaultKeyboard(ctx)
         ctx.wizard.state.telegramId = ctx.message.chat.id;
         if (!ctx.wizard.state.firstname && authValidator.name(ctx.message.text)) {
             ctx.wizard.state.firstname = ctx.message.text;
@@ -21,6 +43,7 @@ module.exports = new Scenes.WizardScene(
         return ctx.wizard.next();
     },
     async (ctx) => {
+        if (ctx.message.text === 'Cancel') return defaultKeyboard(ctx)
         if (!ctx.wizard.state.lastname && authValidator.name(ctx.message.text)) {
             ctx.wizard.state.lastname = ctx.message.text;
         } else if (!ctx.wizard.state.lastname) {
@@ -31,6 +54,7 @@ module.exports = new Scenes.WizardScene(
         return ctx.wizard.next();
     },
     async (ctx) => {
+        if (ctx.message.text === 'Cancel') return defaultKeyboard(ctx)
         if (!ctx.wizard.state.email && authValidator.email(ctx.message.text)) {
             ctx.wizard.state.email = ctx.message.text;
         } else if (!ctx.wizard.state.email) {
@@ -48,6 +72,11 @@ module.exports = new Scenes.WizardScene(
         return ctx.wizard.next()
     },
     async (ctx) => {
+        if (ctx.message && ctx.message.text === 'Cancel') return defaultKeyboard(ctx)
+        if (!ctx.callbackQuery) {
+            ctx.wizard.back();
+            return ctx.wizard.steps[ctx.wizard.cursor](ctx);
+        }
         ctx.wizard.state.course = ctx.callbackQuery.data;
         const user = new User();
         await user.signUp(ctx.wizard.state);

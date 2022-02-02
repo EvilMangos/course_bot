a54d27d858329a968ae6ac86a834359548666ad8
EvilMangos <59839253+EvilMangos@users.noreply.github.com>
2022-02-02 19:10:22 +0200

    CB-301: implement coursesPrograms

--- src/commands/account.js
+++ src/commands/account.js
@@ -1,6 +1,9 @@
 const User = require('../services/user/index')
+const checkAccess = require("../utils/checkAccess");
 
 module.exports = async (ctx) => {
+    const isAccess = await checkAccess(ctx, [process.env.USER]);
+    if (!isAccess) return ctx.reply('Access denied');
     const userService = new User();
     const user = await userService.getById(ctx.chat.id);
     const account = await userService.getAccount(user);
